56f4cb38db45df1b676c3b57f96cebfea19f614e
mic9-morinaka <67672299+mic9-morinaka@users.noreply.github.com>
2021-07-25 17:03:11 +0900

    routing修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -15,17 +13,7 @@ export default function App() {
         <Link to="/page2">Page2</Link>
         <br />
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1ページです</h1>
+      <Link to="/page1/detailA">detailA</Link>
+      <br />
+      <Link to="/page1/detailB">detailB</Link>
+      <br />
     </div>
   );
 };
