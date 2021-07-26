6b3e79f6297a9b1557993860b43b9613f9747f8d
mic9-morinaka <67672299+mic9-morinaka@users.noreply.github.com>
2021-07-26 23:22:44 +0900

    クエリパラメーターテスト

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Page2 = () => {
   return (
     <div>
       <h1>Page２ページです</h1>
+      <Link to="/page2/999?name=aaaaa">URL Parameter</Link>
     </div>
   );
 };
